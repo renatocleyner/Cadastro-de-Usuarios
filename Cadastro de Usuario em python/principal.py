85e54be0230cd57b26e4fae00c4ba4829064032c
Renato Cleyner Ferreira <80468714+renatocleyner@users.noreply.github.com>
2023-02-04 08:54:27 -0300

    implementação do metodo deletar usuarios

--- Cadastro de Usuario em python/principal.py
+++ Cadastro de Usuario em python/principal.py
@@ -1,116 +1,120 @@
-from operator import index
-from model.usuarioModel import Usuario
+from model.usuario import Usuario
 
-listaUsuario = []
+listaUsuarios = list()
 
-def telaInicial():
-    opcao = int(input(
-    '''\n
-    Sejam bem-vindos!
-    O que deseja fazer?
-        1 - Login;
-        2 - Cadastrar Usuário;
-        3 - Listar Usuários;
-        4 - Deletar usuários;
-        5 - Sair
-     Sua escolha: '''))
+
+
+def Menu():
     
+    opcao = int(input('''\n
+    ##############  Sistema de Login ###############
+    ##############  Sejam bem-vindos! ############## 
+    #    O que deseja fazer?
+    #    1 - Login;
+    #    2 - Cadastrar Usuário;
+    #    3 - Listar Usuários;
+    #    4 - Deletar usuários;
+    #    5 - Sair
+    ############## ################## ############## 
+    Sua escolha: '''))
+
     if opcao == 5:
-        return
+        TelaSair()
     elif opcao == 1:
-        telaLogin()
+        TelaLogin()
     elif opcao == 2:
-        telaNovoUsuario()
+        TelaCadastro()
     elif opcao == 3:
-        telaListarUsuario()
+        TelaListandoUsuarios()
     elif opcao == 4:
-        telaDeletarUsuario()
+        deletandoUsuarios()
     else:
-        print('Digite uma opção válida: ')
-        telaInicial()      
-    
-def mostrarListaUsuario():
-    print('#' * 30)
-    for usuario in listaUsuario:
-        #nome, nUsuario, senha = usuario.values()
-        #print(f'{nome} | {nUsuario} | {senha}')
-        print(usuario)
-    
-    print('#' * 30)
+        print('digite opção valida')
+        Menu()
 
-def telaListarUsuario():
-    
-    mostrarListaUsuario()
+def TelaSair():
+    print('''Obrigado por acessar o sistema''')
+    exit()
 
-    telaInicial()
+def TelaCadastro():
+    print("\n++++++++ Cadastro de Usuario ++++++++")
+    novoUsuario = str(input("Digite o nome do novo usuario: "))
+    novaSenha = str(input("Digite a nova senha: "))
+    usuarioCadastrado = Usuario(novoUsuario, novaSenha)
+    dicio = dict({'usuario': usuarioCadastrado.usuario , 'senha': usuarioCadastrado.senha })
 
-def getLoginUsuario(usuarioPassado, senhaPassada):
-    usuarioProcurado = usuarioPassado
-    senhaProcurada = senhaPassada
-    
-    for usuario in listaUsuario:
-        nome, nUsuario, senha = usuario.values()
-
-        if ( usuarioProcurado == nUsuario) and (senhaProcurada == senha):
-            return print(f'Seja bem-vindo(a) {nUsuario} !')
-        elif(usuarioProcurado == nUsuario) and (senhaProcurada != senha):
-            print('Senha inválida.')
-        elif(usuarioProcurado != nUsuario) and (senhaProcurada == senha):
-            print('Usuário não encontrado.')
-        else:
-            print('Usuário e senha não conferem.')
-    return telaInicial()
-    
-
-def telaDeletarUsuario():
-
-    mostrarListaUsuario()
-
-
-    print("\n++++++++ Deletando Usuario ++++++++")
-    loginUsuario = str(input('Qual usuario deseja excluir: '))
-    
-    for user in listaUsuario:
-        if user['usuario'] == loginUsuario:
-            listaUsuario = listaUsuario['usuario'].pop(user)
-            
-    
-    mostrarListaUsuario()
-    
-        
-   
-
-def telaLogin():
-    print("\n++++++++ Acesso ao Sistema ++++++++")
-    loginUsuario = str(input('Digite um usuario: '))
-    loginSenha = str(input('Digite a senha: '))
-    
-    getLoginUsuario(loginUsuario, loginSenha)
-    
-
-def telaNovoUsuario():
-    
-    print("\n++++++++ Novo Usuário ++++++++")
-    nome = str(input('Digite o nome completo do usuário: '))
-    usuario = str(input('Digite o Usuário: '))
-    senha = str(input('Digite a senha: '))
-    novoUsuario = Usuario(nome, usuario, senha)
-    dicio = {'nome': novoUsuario.nome, 'usuario': novoUsuario.usuario, 'senha': novoUsuario.senha}
-    
     print("\n++++++++ Salvando ++++++++")
-    decisao = int(input(
-        '''Deseja salvar esse usuario?
-           1 - Sim;
-           2 - Não;
+    descisao = int(input(
+        '''
+        Deseja salvar esse usuario?
+        1- Sim;
+        2- Nao;
         '''))
+    if descisao == 2:
+        return
+    else:
+        listaUsuarios.append(dicio.copy())
+        print(f'Usuario {usuarioCadastrado.usuario} salvo com sucesso')
 
-    if decisao == 1:
-        listaUsuario.append(dicio.copy())
-        print("\n++++++++  ++++++++")
-        print(f'Usuário {usuario} salvo com sucesso')
-       
+    Menu()
+
+def consultandoUsuario(usuarioPassado, senhaPassada):
+    usuarioConsultado = usuarioPassado
+    senhaConsultada = senhaPassada
     
-    return telaInicial()
+    for usuario in listaUsuarios:
+        user, senha = usuario.values()
+        if(usuarioConsultado == user) and (senhaConsultada == senha):
+            return print(f'Seja bem-vindo(a) {user} !')
+        elif(usuarioConsultado == user) and (senhaConsultada != senha):
+            print("Senha Invalida")
+        elif(usuarioConsultado != user) and (senhaConsultada == senha):
+            print('Usuario não encontrado')
+        else:
+            print("Usuario e senha não conferem")
+    return Menu()        
 
 
-telaInicial()
+def TelaLogin():
+    print("\n++++++++ Acesso ao Sistema ++++++++")
+    usuarioLogado = str(input("Digite um usuario: "))
+    senhaLogado = str(input("Digite a senha: "))
+
+    consultandoUsuario(usuarioLogado, senhaLogado)
+
+def listandoUsuarios():
+    print('#'*30)
+    print('++++++++ Listando Usuarios ++++++++')
+    for usuario in listaUsuarios:
+        print(usuario)
+        
+    Menu()
+
+def deletandoUsuarios():
+    usuarioDeletado = str(input('Digite o nome do usuario a ser deletado: '))
+
+    for usuario in range(len(listaUsuarios)):
+        if listaUsuarios[usuario]['usuario'] == usuarioDeletado:
+            del listaUsuarios[usuario]
+            break            
+            
+
+    # usuarioParaDeletar = next((u for u in listaUsuarios if u['usuario']== usuarioDeletado), None)    
+    # print(usuarioParaDeletar)
+    # del usuarioParaDeletar 
+
+    
+    # for usuario in listaUsuarios:
+    #     if usuario == usuarioDeletado:
+    #         listaUsuarios.remove['usuario'](usuarioDeletado)
+ 
+    print(f"O usuario {usuarioDeletado} deletado com sucesso")       
+    listandoUsuarios()
+    Menu()
+    
+
+def TelaListandoUsuarios():
+    listandoUsuarios()
+    Menu()
+
+Menu()
